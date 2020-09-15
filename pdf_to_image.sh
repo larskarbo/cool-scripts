1da89440a6a32069c53878c4fbc160730d7116c3
Lars Karbo <mail@larskarbo.no>
2020-09-15 15:37:26 +0200

    upd

--- pdf_to_image.sh
+++ pdf_to_image.sh
@@ -1,5 +1,14 @@
 cd ~/Downloads
 FILENAME=$(ls -Art | grep pdf | grep -v comp | tail -n 1)
 echo $FILENAME
-convert -density 150 "$FILENAME" -quality 90 "$FILENAME.png"
+
+
+
+### OLD:
+# convert -density 150 "$FILENAME" -quality 90 "$FILENAME.png"
+
+
 ## nb det her er kanskje bedre: https://askubuntu.com/questions/50170/how-to-convert-pdf-to-image/50180#50180
+
+### NEW:
+pdftoppm "$FILENAME" "$FILENAME" -png -r 300
